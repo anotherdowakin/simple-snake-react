7d2f0699b765b86baaa57f6327c6f5f5c995bbde
Tsvihunenko, Vladimir [PS] <vladimir.tsvihunenko@cengage.com>
2023-06-19 18:50:20 +0300

    handle logic related to fieldSize setting

--- src/pages/SettingsPage.jsx
+++ src/pages/SettingsPage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useRef, useState} from "react";
 
 import {useSettings} from "../components/settings/context/SettingsContext";
 import {DIFFICULTIES_LEVELS} from "../components/settings/const/SettingsConsts";
@@ -6,6 +6,8 @@ import './css/settings.css';
 
 const SettingsPage = () => {
     const {settings, setSettings} = useSettings();
+    const fieldSizeRef = useRef();
+    const [fieldSize, setFieldSize] = useState(settings.fieldSize);
 
     const changeDifficultyLevel = (newDifficultyLevelTitle) => {
         const newDifficultyLevel = DIFFICULTIES_LEVELS.find(setting => setting.title === newDifficultyLevelTitle);
@@ -14,10 +16,24 @@ const SettingsPage = () => {
         }
     }
 
-    const changeFieldSize = (fieldSize) => {
-        if (fieldSize <= 50 && fieldSize >= 5) {
-            setSettings({fieldSize});
+    const setFieldInput = (value) => {
+        const inputValue = value.target.value;
+        setFieldSize(inputValue);
+    }
+
+    const changeFieldSize = () => {
+        let newFieldSize = fieldSizeRef.current.value;
+
+        const isOutOfRange = newFieldSize > 50 || newFieldSize < 5;
+
+        if (isOutOfRange) {
+            newFieldSize = fieldSize > 50 ? 50 : 5;
+        } else {
+            newFieldSize = fieldSize;
         }
+
+        setFieldSize(newFieldSize);
+        setSettings({fieldSize: newFieldSize});
     }
 
     return <div className='pageBody'>
@@ -32,8 +48,8 @@ const SettingsPage = () => {
                 })}
             </select>
             <h2>Field size (min - 5, max - 50):</h2>
-            <input type='number' value={settings.fieldSize} min={5} max={50}
-                   onChange={fieldSize => changeFieldSize(fieldSize.target.value)}></input>
+            <input ref={fieldSizeRef} type='number' value={fieldSize} min={5} max={50} onChange={setFieldInput}></input>
+            <button onClick={changeFieldSize}>Apply</button>
         </div>
     </div>
 }
